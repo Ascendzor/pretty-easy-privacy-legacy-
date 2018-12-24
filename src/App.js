eb3d86714dcea24a7ed9caa2d7eeb4208928231b
Ascendzor <wow.troymare@gmail.com>
2018-12-24 23:06:14 +1300

    better joyride tutorial

--- src/App.js
+++ src/App.js
@@ -31,6 +31,38 @@ const joyrideSteps = [
     placement: 'center',
     target: '.App-header',
     styles: {options: {width: 800}}
+  },
+  {
+    content: <div>
+      <div>There is one new concept you will have to wrap your head around</div>
+      <h2>The keypair</h2>
+      <div style={{marginBottom: 10}}>
+        <div>The <span style={{fontWeight: 'bold'}}>keypair</span> consists of two keys: <span style={{fontWeight: 'bold'}}>publicKey</span>, <span style={{fontWeight: 'bold'}}>privateKey</span></div>
+      </div>
+      <div style={{marginBottom: 10}}>
+        These two keys are mathematically related, such that a message may be encrypted using the <span style={{fontWeight: 'bold'}}>publicKey</span>, and then that encrypted message may be decrypted using the <span style={{fontWeight: 'bold'}}>privateKey</span>.
+      </div>
+      <div>
+        On the next page, there will be an example use case
+      </div>
+    </div>,
+    placement: 'center',
+    target: '.App-header',
+    styles: {options: {width: 800}}
+  },
+  {
+    content: <div>
+      <h2>Example - Alice writing a private message to Bob</h2>
+      <div>Suppose Alice wanted to write a private message to bob.</div>
+      <div>Alice will take Bobs <span style={{fontWeight: 'bold'}}>PublicKey</span>, and then use that to encrypt her message.</div>
+      <div>Alice will then have an encrypted message, that Bob can decrypt.</div>
+      <div>Alice sends Bob that encrypted message.</div>
+      <div>Bob now has the encrypted message, and Bob still has his <span style={{fontWeight: 'bold'}}>PrivateKey</span>.</div>
+      <div>Bob is able to decrypt the message.</div>
+    </div>,
+    placement: 'center',
+    target: '.App-header',
+    styles: {options: {width: 800}}
   }
 ]
 
