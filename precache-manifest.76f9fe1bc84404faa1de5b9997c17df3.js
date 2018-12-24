0a1c2d266d44ad4baf013386bbe921d09e8e31c6
Ascendzor <wow.troymare@gmail.com>
2018-12-24 21:58:25 +1300

    Updates

---
+++ precache-manifest.76f9fe1bc84404faa1de5b9997c17df3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pretty-easy-privacy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fbd4d461e164571362e7",
+    "url": "/pretty-easy-privacy/static/js/runtime~main.fbd4d461.js"
+  },
+  {
+    "revision": "287761c04112d4426564",
+    "url": "/pretty-easy-privacy/static/js/main.287761c0.chunk.js"
+  },
+  {
+    "revision": "4117542a95b4fa116d41",
+    "url": "/pretty-easy-privacy/static/js/1.4117542a.chunk.js"
+  },
+  {
+    "revision": "287761c04112d4426564",
+    "url": "/pretty-easy-privacy/static/css/main.1af6c1b9.chunk.css"
+  },
+  {
+    "revision": "f55de7dd7e42346a946f301a9c23c700",
+    "url": "/pretty-easy-privacy/index.html"
+  }
+];
